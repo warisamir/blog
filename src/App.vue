dbd176c23467f14ed8c38967c52141f88d26fcf6
warisamir <warisamir1918@gmail.com>
2024-03-08 22:39:06 +0530

    just having fun with lady on women's day

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
+  <ConditionRendering/>
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>
   <HomeWork ></HomeWork>
 <laonAmount></laonAmount>
-<InputForm></InputForm> -->
-<HomeLate/>
+<InputForm></InputForm>
+<HomeLate/> -->
 </template>
 
 <script>
-import HomeLate from './components/HomeLate.vue'
+import ConditionRendering from './components/ConditionRendering.vue'
+// import HomeLate from './components/HomeLate.vue'
 // import InputForm from './components/InputForm.vue'
 // import HelloWorld from './components/HelloWorld.vue'
-// HelloWorld,
-    // HomeWork,
-    // InputForm,
-    // laonAmount
 // import HomeWork from './components/Home.vue'
 // import laonAmount from './components/laonAmount.vue';
+
 export default {
   name: 'App',
   components: {
-    HomeLate
+    // HelloWorld,
+    // HomeWork,
+    // InputForm,
+    ConditionRendering,
+    // laonAmount,
+    // HomeLate
   }
 }
 </script>
---
+++ src/components/ConditionRendering.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+      <h1 v-if="show">If Else Condition</h1>
+      <h2 v-else>Else Condition</h2>
+      <button v-on:click="toggleShow" class="btn">Click me</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "ConditionRendering",
+    data() {
+      return {
+        show: false,
+        filterApplied: false // Add a data property to track if filter is applied
+      };
+    },
+    methods: {
+      toggleShow() {
+        this.show = !this.show;
+        this.filterApplied = !this.filterApplied; // Toggle filter
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: yellowgreen;
+  }
+  
+  .btn {
+    border: 2px solid transparent;
+    width: 70%;
+    height: 580px;
+    border-radius: 50%;
+    background-image: url(https://plus.unsplash.com/premium_photo-1709059961525-b9d83a3e476b?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMnx8fGVufDB8fHx8fA%3D%3D);
+  }
+  
+  /* Apply filter when filterApplied is true */
+  .btn:active {
+    -webkit-filter: grayscale(100%);
+    filter: grayscale(100%);
+  }
+  </style>
