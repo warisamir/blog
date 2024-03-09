310c86fb3d2140151a8ff72438dc6b39b21314da
warisamir <warisamir1918@gmail.com>
2024-03-09 12:10:22 +0530

    HtmlBinding

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,19 @@
 <template>
-  <!-- <ConditionRendering/> -->
-  <!-- <PassData/> -->
-  <!-- <ForLoop/> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <!-- <HomeWork ></HomeWork> -->
-<!-- <laonAmount></laonAmount>
-<InputForm></InputForm>
-<HomeLate/> -->
+
 <h2>Parent Component</h2>
-<ul>
-  <li v-for="item in users" :key="item.name">
-  <!-- {{ item.name }} --- {{ item.email }} -->
-  <ChildReuse :data="item" :getData="getData"/>
-  </li>
 
-</ul>
-
+<HtmlBind/>
 </template>
 
 <script>
-// import ConditionRendering from './components/ConditionRendering.vue'
-// import  ForLoop from './components/ForLoop.vue'
-// import PassData from './components/PassData.vue'
-// import HomeLate from './components/HomeLate.vue'
-// import InputForm from './components/InputForm.vue'
-// import HelloWorld from './components/HelloWorld.vue'
-// import HomeWork from './components/Home.vue'
-// import laonAmount from './components/laonAmount.vue';
-import ChildReuse from './components/ChildReuse'
+import HtmlBind from './components/HtmlBind.vue';
+
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
-    // HomeWork,
-    // InputForm,
-    // ConditionRendering,
-    // PassData,
-    // HomeWork,
-    // ForLoop,
-    // laonAmount,
-    // HomeLate
-    ChildReuse,
+    HtmlBind,
   },
-  data(){
-    return {
-      users:[
-        {name:'anil',email:'warisamir1919@gmail.com'},
-        {name:'rock',email:'warisamir1919@gmail.com'},
-        {name:'krk',email:'warisamir1919@gmail.com'},
-        {name:'srk',email:'warisamir1919@gmail.com'},
-      ],
-    }
-  },
-  methods:{
-        getData(name){
-            alert(name)
-        }
-    }
+  
 }
 </script>
 
--- src/components/HtmlBind.vue
+++ src/components/HtmlBind.vue
@@ -1,7 +1,7 @@
 import { default } from '../App.vue';
 <template>
     <h1>{{ text }}</h1>
-    <div v-html="tag +tag2">
+    <div v-html="tag +tag2+tag3">
         </div>
     </template>
     <script>
@@ -12,6 +12,7 @@ import { default } from '../App.vue';
                 text:"Waris Amir",
                 tag:'<h2>anil sidhu</h2>',
                 tag2:'<h1>Peter</h1>',
+                tag3:'<h3>Ronald</h3>'
             }
         }
     }
