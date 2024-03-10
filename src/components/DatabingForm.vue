8259fd0818c3894092318631549cf53a8aa21a76
warisamir <warisamir1918@gmail.com>
2024-03-10 11:40:50 +0530

    simple form

--- src/components/DatabingForm.vue
+++ src/components/DatabingForm.vue
@@ -1,12 +1,26 @@
 <template>
+    <p>simple form{{ form }}} </p>
 <form>
-<label> Email </label>
-<input type="email" placeholder="enter email" v-model="email"/>
+<label> Email : </label>
+<input type="text" placeholder="enter email" v-model="form.email"/>
 <br/><br/>
-<label> Password</label>
-<input type="password" placeholder="password" v-model="password"/> 
+<label> Password : </label>
+<input type="password" placeholder="enter password" v-model="form.password"/> 
 <br/> <br/>
-<button v-on:click="login" type="button">click</button>
+<select v-model="form.country">
+    <option value="">India</option>
+    <option value="">USA</option>
+</select>
+<br/><br/>
+<label> Gender : </label>
+<input type="radio" name="gender" value="male" v-model="form.gender">Male
+<input type="radio" name="gender" value="female" v-model="form.gender"/>female
+<br/><br/>
+<label> Tech : </label>
+<input type="radio" name="tech" value="Java" v-model="form.tech">Java
+<input type="radio" name="tech" value=".Net" v-model="form.tech"/>.Net
+<br/><br/>
+<button v-on:click="submit" type="button">login</button>
 </form>
 </template>
 
@@ -16,13 +30,17 @@ export default {
     name:"DatabingForm",
 data(){
     return {
-        email:"",
-        password:""
+        form:{
+        email:'',
+        password:'',
+        country:'',
+        gender:'',
+        }
     }
 },
 methods:{
-    login(){
-        console.warn("function",this.email,"password=",this.password)
+    submit(){
+        console.warn(this.form)
     }
 }
 }
